6667a80ab5ae328fba28bdac572148cc90a1bbf5
Colin Shields <N02433728@hawkmail.newpaltz.edu>
2014-03-13 16:11:52 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmberGameSuite::Application.config.secret_key_base = '6dd4ccabb957323deb5c45d1d20fa34ae37157588e6a92257a74f262d865157674a2d873d8a69b530d5bf6fb3a4c5114bda21b5cdfdc84106cde0d4f2e1485c6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EmberGameSuite::Application.config.session_store :cookie_store, key: '_ember_game_suite_session'
